8b6bcc28c317192131cf0e7ed76ceff57c0b9f13
Ian Souza <ianfmsouza@gmail.com>
2025-12-06 12:29:31 -0300

    chore: att seed and schema for users

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,12 +1,18 @@
-import { text, timestamp } from "drizzle-orm/pg-core";
+import { text, timestamp, uuid, pgTable, pgEnum } from "drizzle-orm/pg-core";
 import { uniqueIndex } from "drizzle-orm/pg-core";
-import { uuid } from "drizzle-orm/pg-core";
-import { pgTable } from "drizzle-orm/pg-core";
+
+export const userRole = pgEnum("user_roles", [
+  "student",
+  "instructor",
+  "manager",
+]);
 
 export const users = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
   name: text().notNull(),
   email: text().notNull().unique(),
+  password: text().notNull(),
+  role: userRole().notNull().default("student"),
 });
 
 export const courses = pgTable("courses", {
