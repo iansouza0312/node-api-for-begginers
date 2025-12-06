1275a2fd0b9f22a350facd400280219c4fd421a9
Ian Souza <ianfmsouza@gmail.com>
2025-12-06 14:18:34 -0300

    chore: add jwt authentication for user login

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,9 +1,9 @@
 // Paulo.Macedo@yahoo.com 123456
-
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { users } from "../database/schema.ts";
 import { z } from "zod";
+import jwt from "jsonwebtoken";
 import { eq } from "drizzle-orm";
 import { verify } from "@node-rs/argon2";
 
@@ -19,11 +19,10 @@ export const loginRoute: FastifyPluginAsyncZod = async (server) => {
           email: z.email(),
           password: z.string(),
         }),
-        // response: {
-        //   201: z
-        //     .object({ courseId: z.uuid() })
-        //     .describe("Returns the ID of the newly created course"),
-        // },
+        response: {
+          200: z.object({ token: z.string() }),
+          400: z.object({ message: z.string() }),
+        },
       },
     },
     async (request, reply) => {
@@ -46,7 +45,16 @@ export const loginRoute: FastifyPluginAsyncZod = async (server) => {
         return reply.status(400).send({ message: "Credenciais inválidas" });
       }
 
-      return reply.status(200).send({ message: "OK" });
+      if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in environment variables");
+      }
+
+      const token = jwt.sign(
+        { sub: user.id, role: user.role },
+        process.env.JWT_SECRET
+      );
+
+      return reply.status(200).send({ token });
     }
   );
 };
