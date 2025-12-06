0ab5f8a18c493db494970690fe15b136a92d1987
Ian Souza <ianfmsouza@gmail.com>
2025-12-05 22:54:00 -0300

    att: Att function to get course by id

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import crypto from "node:crypto";
 import { db } from "./src/database/client.ts";
 import { courses } from "./src/database/schema.ts";
+import { eq } from "drizzle-orm";
 
 const server = fastify({
   logger: {
@@ -32,22 +33,25 @@ server.get("/courses", async (request, reply) => {
   return reply.send({ courses: result });
 });
 
-// server.get("/courses/:id", (request, reply) => {
-//   type Params = {
-//     id: string;
-//   };
+server.get("/courses/:id", async (request, reply) => {
+  type Params = {
+    id: string;
+  };
 
-//   const params = request.params as Params;
-//   const courseId = params.id;
+  const params = request.params as Params;
+  const courseId = params.id;
 
-//   const course = courses.find((course) => course.id === courseId);
+  const result = await db
+    .select()
+    .from(courses)
+    .where(eq(courses.id, courseId));
 
-//   if (course) {
-//     return { course };
-//   }
+  if (result.length > 0) {
+    return { course: result[0] };
+  }
 
-//   return reply.status(404).send();
-// });
+  return reply.status(404).send();
+});
 
 // server.post("/courses", (request, reply) => {
 //   type Body = {
