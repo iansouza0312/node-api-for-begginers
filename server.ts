7b9cac54b20e3ab70ffb618f68b7d6e68963e07a
Ian Souza <ianfmsouza@gmail.com>
2025-12-05 22:36:07 -0300

    chore: get courses route

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import fastify from "fastify";
 import crypto from "node:crypto";
+import { db } from "./src/database/client.ts";
+import { courses } from "./src/database/schema.ts";
 
 const server = fastify({
   logger: {
@@ -13,50 +15,57 @@ const server = fastify({
   },
 });
 
-const courses = [
-  { id: "1", title: "Learn ReactJS" },
-  { id: "2", title: "Learn VueJS" },
-  { id: "3", title: "Learn Angular" },
-];
+// const courses = [
+//   { id: "1", title: "Learn ReactJS" },
+//   { id: "2", title: "Learn VueJS" },
+//   { id: "3", title: "Learn Angular" },
+// ];
 
-server.get("/courses", (request, reply) => {
-  return reply.send({ courses });
+server.get("/courses", async (request, reply) => {
+  const result = await db
+    .select({
+      id: courses.id,
+      title: courses.title,
+    })
+    .from(courses);
+
+  return reply.send({ courses: result });
 });
 
-server.get("/courses/:id", (request, reply) => {
-  type Params = {
-    id: string;
-  };
+// server.get("/courses/:id", (request, reply) => {
+//   type Params = {
+//     id: string;
+//   };
 
-  const params = request.params as Params;
-  const courseId = params.id;
+//   const params = request.params as Params;
+//   const courseId = params.id;
 
-  const course = courses.find((course) => course.id === courseId);
+//   const course = courses.find((course) => course.id === courseId);
 
-  if (course) {
-    return { course };
-  }
+//   if (course) {
+//     return { course };
+//   }
 
-  return reply.status(404).send();
-});
+//   return reply.status(404).send();
+// });
 
-server.post("/courses", (request, reply) => {
-  type Body = {
-    title: string;
-  };
+// server.post("/courses", (request, reply) => {
+//   type Body = {
+//     title: string;
+//   };
 
-  const courseId = crypto.randomUUID();
-  const body = request.body as Body;
-  const courseTitle = body.title;
+//   const courseId = crypto.randomUUID();
+//   const body = request.body as Body;
+//   const courseTitle = body.title;
 
-  if (!courseTitle) {
-    return reply.status(422).send({ error: "Title is a required field" });
-  }
+//   if (!courseTitle) {
+//     return reply.status(422).send({ error: "Title is a required field" });
+//   }
 
-  courses.push({ id: courseId, title: courseTitle });
+//   courses.push({ id: courseId, title: courseTitle });
 
-  return reply.status(201).send({ courseId });
-});
+//   return reply.status(201).send({ courseId });
+// });
 
 server.listen({ port: 3333 }).then(() => {
   console.log("Server is running on http://localhost:3333");
